8614827731baa3e6086b2b2b4b1254c5c1f9e9d1
esuvs <esuvs@2eb06014-c84b-0410-ad17-af83cdd4cbf1>
2009-01-04 18:37:46 +0000

    Added rotation support to skyboxes. Tidying up and commenting.
    git-svn-id: https://ogreaddons.svn.sourceforge.net/svnroot/ogreaddons/trunk/QtOgreFramework@2595 2eb06014-c84b-0410-ad17-af83cdd4cbf1

--- QtOgre/include/DotSceneHandler.h
+++ QtOgre/include/DotSceneHandler.h
@@ -5,17 +5,20 @@
 
 #include <QPair>
 #include <QStack>
-#include <QXmlSimpleReader>
 #include <QXmlDefaultHandler>
 
 ///NOTE: Any cameras found in the scene file are added to the scene manager, but any existing
-///Cameras are not removed. This is because they me be in use by viewports.
+///Cameras are not removed. This is because they may be in use by viewports.
 ///See http://www.ogre3d.org/docs/api/html/classOgre_1_1SceneManager.html#a5b2047b5740b691b0e636d57f2dba7e
 class DotSceneHandler : public QXmlDefaultHandler
 {
 public:
 	DotSceneHandler(Ogre::SceneManager* sceneManager);
 
+	bool startElement(const QString &, const QString &, const QString& qName, const QXmlAttributes &attributes);
+	bool endElement(const QString &, const QString &, const QString& qName);
+
+private:
 	Ogre::Camera* handleCamera(const QXmlAttributes &attributes);
 	Ogre::Entity* handleEntity(const QXmlAttributes &attributes);
 	Ogre::SceneNode* handleNode(const QXmlAttributes &attributes);
@@ -26,17 +29,25 @@ public:
 	void* handleScene(const QXmlAttributes &attributes);
 	Ogre::SceneNode* handleSkyBox(const QXmlAttributes &attributes);
 
-	bool startElement(const QString &, const QString &, const QString& qName, const QXmlAttributes &attributes);
-	bool endElement(const QString &, const QString &, const QString& qName);
-
-private:
 	bool convertWithDefault(const QString& inputString, bool defaultVal);
 	double convertWithDefault(const QString& inputString, double defaultVal);
 	float convertWithDefault(const QString& inputString, float defaultVal);
 	int convertWithDefault(const QString& inputString, int defaultVal);
 	QString convertWithDefault(const QString& inputString, char* defaultVal);
 
+	///The scene manager into which the XML is being loaded
 	Ogre::SceneManager* mSceneManager;
+	/**
+	There are primarily two types of XML nodes we will encounter. Some (like 'entity', 'camera', etc) are
+	actual Ogre objects which can exist in the scene. Others (like 'rotation', 'scale', etc) are transformations
+	which should be applied to the Ogre object with the 'most local scope'. As we enter and exit XML nodes we
+	push and pop the corresponding Ogre objects onto the stack below. Then, when we encounter a transformation,
+	we just apply it to whatever is at the top of the stack.
+
+	The apporach works, but there might be a better and/or more type safe way to do it. Because any kind of Ogre
+	object could be on the stack we store them using void pointers and then use the string to cast to the correct
+	type at runtime.
+	*/
 	QStack< QPair< QString, void* > > mParentOgreObjects;
 };
 
--- QtOgre/source/DotSceneHandler.cpp
+++ QtOgre/source/DotSceneHandler.cpp
@@ -13,10 +13,14 @@ bool DotSceneHandler::startElement(const QString & /* namespaceURI */,
 								   const QString &qName,
 								   const QXmlAttributes &attributes)
 {
+	//For debugging
 	qDebug((QString("Starting Element ") + qName).toStdString().c_str());
 
+	//This will hold a pointer to any ogre object
+	//created when processing this XML node.
 	void* ogreObject = 0;
 	
+	//Call the appropriate helper function depending on the XML node type.
 	if(qName == "camera")
 	{
 		ogreObject = handleCamera(attributes);
@@ -54,6 +58,8 @@ bool DotSceneHandler::startElement(const QString & /* namespaceURI */,
 		ogreObject = handleSkyBox(attributes);
 	}
 
+	//If an Ogre object was created then add it to the stack.
+	//Later transformations will be applied to this top object.
 	if(ogreObject != 0)
 	{		
 		QPair< QString, void* > pair(qName, ogreObject);
@@ -67,21 +73,18 @@ bool DotSceneHandler::endElement(const QString & /* namespaceURI */,
 								 const QString & /* localName */,
 								 const QString &qName)
 {
+	//For debugging
 	qDebug((QString("Ending Element ") + qName).toStdString().c_str());
 
-	if(qName == "camera")
-	{
-		mParentOgreObjects.pop();
-	}
-	if(qName == "entity")
-	{
-		mParentOgreObjects.pop();
-	}
-	if(qName == "node")
-	{
-		mParentOgreObjects.pop();
-	}
-	if(qName == "skyBox")
+	//If the XML node causes an Ogre object to be added to the stack,
+	//we remove it to expose the one which was there previously.
+	if
+	(
+		(qName == "camera") ||
+		(qName == "entity") ||
+		(qName == "node") ||
+		(qName == "skyBox")
+	)
 	{
 		mParentOgreObjects.pop();
 	}
@@ -92,6 +95,7 @@ bool DotSceneHandler::endElement(const QString & /* namespaceURI */,
 Ogre::Camera* DotSceneHandler::handleCamera(const QXmlAttributes &attributes)
 {
 	QPair< QString, void* > topPair = mParentOgreObjects.top();
+	//Camera can be attached to the root node ('nodes'), or one of it's children ('node').
 	if((topPair.first == "node") || (topPair.first == "nodes"))
 	{
 		Ogre::SceneNode* node = static_cast<Ogre::SceneNode*>(topPair.second);
@@ -107,6 +111,7 @@ Ogre::Camera* DotSceneHandler::handleCamera(const QXmlAttributes &attributes)
 Ogre::Entity* DotSceneHandler::handleEntity(const QXmlAttributes &attributes)
 {
 	QPair< QString, void* > topPair = mParentOgreObjects.top();
+	//Entity can be attached to the root node ('nodes'), or one of it's children ('node').
 	if((topPair.first == "node") || (topPair.first == "nodes"))
 	{
 		Ogre::SceneNode* node = static_cast<Ogre::SceneNode*>(topPair.second);
@@ -121,6 +126,7 @@ Ogre::Entity* DotSceneHandler::handleEntity(const QXmlAttributes &attributes)
 Ogre::SceneNode* DotSceneHandler::handleNode(const QXmlAttributes &attributes)
 {
 	QPair< QString, void* > topPair = mParentOgreObjects.top();
+	//Node can be attached to the root node ('nodes'), or one of it's children ('node').
 	if((topPair.first == "node") || (topPair.first == "nodes"))
 	{
 		Ogre::SceneNode* node = static_cast<Ogre::SceneNode*>(topPair.second);
@@ -131,6 +137,7 @@ Ogre::SceneNode* DotSceneHandler::handleNode(const QXmlAttributes &attributes)
 
 Ogre::SceneNode* DotSceneHandler::handleNodes(const QXmlAttributes &attributes)
 {
+	//We only expect a single 'nodes' element in the XML, and this should correspond to the root.
 	return mSceneManager->getRootSceneNode();
 }
 
@@ -148,17 +155,26 @@ void* DotSceneHandler::handlePosition(const QXmlAttributes &attributes)
 		node->setPosition(attributes.value("x").toFloat(), attributes.value("y").toFloat(), attributes.value("z").toFloat());
 	}
 
+	//We haven't created any Ogre objects, so nothing to return
 	return 0;
 }
 
 void* DotSceneHandler::handleRotation(const QXmlAttributes &attributes)
 {
 	QPair< QString, void* > topPair = mParentOgreObjects.top();
+	if(topPair.first == "skyBox")
+	{
+		//Finding a skybox actually adds a SceneNode to the stack.
+		Ogre::SceneNode* node = static_cast<Ogre::SceneNode*>(topPair.second);
+		node->setOrientation(attributes.value("qw").toFloat(), attributes.value("qx").toFloat(), attributes.value("qy").toFloat(), attributes.value("qz").toFloat());
+	}
 	if(topPair.first == "node")
 	{
 		Ogre::SceneNode* node = static_cast<Ogre::SceneNode*>(topPair.second);
 		node->setOrientation(attributes.value("qw").toFloat(), attributes.value("qx").toFloat(), attributes.value("qy").toFloat(), attributes.value("qz").toFloat());
 	}
+
+	//We haven't created any Ogre objects, so nothing to return
 	return 0;
 }
 
@@ -170,13 +186,15 @@ void* DotSceneHandler::handleScale(const QXmlAttributes &attributes)
 		Ogre::SceneNode* node = static_cast<Ogre::SceneNode*>(topPair.second);
 		node->setScale(attributes.value("x").toFloat(), attributes.value("y").toFloat(), attributes.value("z").toFloat());
 	}
+
+	//We haven't created any Ogre objects, so nothing to return
 	return 0;
 }
 
 void* DotSceneHandler::handleScene(const QXmlAttributes &attributes)
 {
+	//Removes everything *except* cameras
 	mSceneManager->clearScene();
-
 	return mSceneManager;
 }
 
@@ -196,11 +214,11 @@ Ogre::SceneNode* DotSceneHandler::handleSkyBox(const QXmlAttributes &attributes)
 
 bool DotSceneHandler::convertWithDefault(const QString& inputString, bool defaultVal)
 {
-	if(inputString.compare("true", Qt::CaseInsensitive))
+	if(inputString.compare("true"))
 	{
 		return true;
 	}
-	if(inputString.compare("false", Qt::CaseInsensitive))
+	if(inputString.compare("false"))
 	{
 		return false;
 	}
